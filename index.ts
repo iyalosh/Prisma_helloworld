96698c649f0c53e2a554d7e4d36c4297eedc4c8c
Iyas Alloush <iyassalloosh@gmail.com>
2019-02-18 11:37:04 +0000

    Successful test for adding several users to the database using the index.ts file see example in https://www.prisma.io/docs/get-started/01-setting-up-prisma-demo-server-TYPESCRIPT-t001/

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ import { prisma } from './generated/prisma-client'
 async function main() {
 
   // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' })
+  const newUser = await prisma.createUser({ name: 'Thomas' })
   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-
+ 
   // Read all users from the database and print them to the console
   const allUsers = await prisma.users()
   console.log(allUsers)
