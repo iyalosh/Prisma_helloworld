a5fcca1832b2ebaaa9930d48113c3a2d8bbdeae2
Iyas Alloush <iyassalloosh@gmail.com>
2019-02-18 12:00:59 +0000

    Successful test for update Alice duplicated to Bob using its id  into the demo database using the index.ts file see example in https://www.prisma.io/docs/get-started/01-setting-up-prisma-demo-server-TYPESCRIPT-t001/

--- index.ts
+++ index.ts
@@ -6,20 +6,27 @@ async function main() {
   // Create a new user called `Alice`
   // const user = await prisma
   // .user({ id: 'cjsa9vft66w550b592f6w0tum' })
-  const usersCalledAllice = await prisma.users(
-    {
-      where: {
-        name: 'Alice'
-      }
-    }
-  )
+  // const usersCalledAllice = await prisma.users(
+  //   {
+  //     where: {
+  //       name: 'Alice'
+  //     }
+  //   }
+  // )
   // const newUser1 = await prisma.createUser({ name: 'Marco' })
   // console.log(`Created new user: ${newUser1.name} (ID: ${newUser1.id})`)
   // Read all users from the database and print them to the console
   // const allUsers = await prisma.users()
   // console.log(allUsers)
   // console.log(user)
-  console.log(usersCalledAllice)
+  // console.log(usersCalledAllice)
+  const updatedUser = await prisma
+  .updateUser({
+    where: { id: 'cjsa99exq6nv90b5921ahyqve' },
+    data: { name: 'Bob' }
+  })
+  const allUsers = await prisma.users()
+  console.log(allUsers)
 }
 
 main().catch(e => console.error(e))
