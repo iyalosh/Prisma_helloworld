8784bcac7b1bd3a421f50caa09366f70885aaaf3
Iyas Alloush <iyassalloosh@gmail.com>
2019-02-18 11:56:35 +0000

    Successful test for filtering user list from the demo database using the index.ts file see example in https://www.prisma.io/docs/get-started/01-setting-up-prisma-demo-server-TYPESCRIPT-t001/

--- index.ts
+++ index.ts
@@ -4,15 +4,22 @@ import { prisma } from './generated/prisma-client'
 async function main() {
 
   // Create a new user called `Alice`
-  const user = await prisma
-  .user({ id: 'cjsa9vft66w550b592f6w0tum' })
-
+  // const user = await prisma
+  // .user({ id: 'cjsa9vft66w550b592f6w0tum' })
+  const usersCalledAllice = await prisma.users(
+    {
+      where: {
+        name: 'Alice'
+      }
+    }
+  )
   // const newUser1 = await prisma.createUser({ name: 'Marco' })
   // console.log(`Created new user: ${newUser1.name} (ID: ${newUser1.id})`)
   // Read all users from the database and print them to the console
   // const allUsers = await prisma.users()
   // console.log(allUsers)
-  console.log(user)
+  // console.log(user)
+  console.log(usersCalledAllice)
 }
 
 main().catch(e => console.error(e))
